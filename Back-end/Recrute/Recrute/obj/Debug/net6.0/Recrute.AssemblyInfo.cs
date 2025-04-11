3af765d5ac45d91525d1b942026b1b9a0a69382c
Erion Gashi <eg60004@ubt-uni.net>
2025-04-11 18:46:24 +0200

    front+back pjesa e login+regjister

--- Back-end/Recrute/Recrute/obj/Debug/net6.0/Recrute.AssemblyInfo.cs
+++ Back-end/Recrute/Recrute/obj/Debug/net6.0/Recrute.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recrute")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f815a71083838f04ff73b7a8e21ad4303dd080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recrute")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recrute")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
